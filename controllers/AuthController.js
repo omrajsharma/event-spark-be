e3791b1da0a1d7f3de09482382b9b82aa7fa42e1
omrajsharma <omraj7413@gmail.com>
2023-10-13 08:47:10 +0530

    feat: profileCheck & logoutUser

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -96,6 +96,33 @@ const login = async (req, res) => {
     }
 }
 
+const profileCheck = async (req, res) => {
+    const { token } = req.cookies;
 
+    if (!token) {
+        res.status(401).json({error: 'Please login'})
+        return
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userDoc = await User.findOne({_id: decoded.id});
+        res.status(200).json({
+            data: {
+                userId: userDoc._id,
+                username: userDoc.username,
+                name: userDoc.name,
+                email: userDoc.email,
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({error: 'Error occured'})
+        return
+    }
+}
 
-module.exports = {register, login}
+const logoutUser = (req, res) => {
+    res.clearCookie('token').status(200).json({success: "User Logged Out"});
+}
+
+module.exports = {register, login, profileCheck, logoutUser}
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {register, login} = require('../controllers/AuthController')
+const {register, login, profileCheck, logoutUser} = require('../controllers/AuthController')
 
 router.post('/register', register)
 router.post('/login', login)
+router.get('/profile', profileCheck)
+router.post('/logout', logoutUser)
 
 module.exports = router;
